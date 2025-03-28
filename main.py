f1565159bd29b2bb38a2409ecf45b9514f0955c3
doctor112-1 <08armaan21singh@gmail.com>
2025-03-28 07:57:57 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from random import randrange
+print(randrange(10))
+
+numbers = [randrange(10), randrange(10), randrange(10), randrange(10)]
+
+print(numbers)
+
+def sort(x):
+    for i in range(len(x)):
+        if i + 1 == len(x):
+            break
+        print("At position " + str(i))
+        print(x[i])
+        print("Next position " + str(i + 1))
+        print("Data at next position " + str(x[i + 1]))
+
+sort(numbers)
