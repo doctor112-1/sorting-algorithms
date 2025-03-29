273ea700e4981056a391502773b12bb78315f193
doctor112-1 <08armaan21singh@gmail.com>
2025-03-29 17:26:31 -0400

    no more recursion

--- bubblesort.py
+++ bubblesort.py
@@ -3,6 +3,8 @@ from random import randrange
 
 numbers = [randrange(10), randrange(10), randrange(10), randrange(10)]
 
+big_numbers = random.sample(range(1, 100001), 100000)
+
 print(numbers)
 
 def check_sorted(x):
@@ -24,17 +26,12 @@ def sort(x):
     if is_sorted == True:
         return True
     else:
-            for i in range(len(x)):
-                if i + 1 == len(x):
-                   break 
-                else:
-                    if x[i] > x[i + 1]:
-                        temp = x[i]
-                        temp1 = x[i + 1]
-                        x[i] = temp1
-                        x[i + 1] = temp
-                        print(x)
-    sort(x)
+            for i in range(len(x) - 1, 0, -1):
+                if x[i] > x[i - 1]:
+                    temp = x[i]
+                    temp1 = x[i + 1]
+                    x[i] = temp1
+                    x[i + 1] = temp
 
 sort(numbers)
-
+print(numbers)
